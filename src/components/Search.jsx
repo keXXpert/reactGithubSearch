4078343422210a21165bf3e675ced9762612ca9c
keXXpert <kexxpert@ya.ru>
2020-06-03 18:31:45 +0300

    added alert toggle on input value

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
 
-const Search = (props) => {
+const Search = () => {
+    const [value, setValue] = useState('')
     const {show} = useContext(AlertContext)
     const onSubmit = evt => {
-        if (evt.key === 'Enter') show ('This is alert')
+        if (evt.key !== 'Enter') return 
+        if (value.trim()) {
+            console.log('Make request with: '+ value);
+            setValue('')
+        } else {
+            show('Please enter a valid search request!')
+        }
     }
 
     return (
         <div className="form-group">
             <input
+                value={value}
                 type="text"
                 className='form-control'
                 placeholder='Enter user nickname...'
+                onChange={e=> setValue(e.target.value)}
                 onKeyPress={onSubmit} />
         </div>
     )
