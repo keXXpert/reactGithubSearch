608fd5f3b12af4323e51e78deda8e5c4de6ad056
keXXpert <kexxpert@ya.ru>
2020-06-03 18:02:47 +0300

    added alert context

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Alert from './components/Alert';
+import AlertState from './context/alert/AlertState';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Alert text="Just a test" type='warning' />
-        <Switch>
-          <Route path='/about' component={About} />
-          <Route path='/profile/:name' component={Profile} />
-          <Route path='/' exact component={Home} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert text="Just a test" type='warning' />
+          <Switch>
+            <Route path='/about' component={About} />
+            <Route path='/profile/:name' component={Profile} />
+            <Route path='/' exact component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const Alert = ({text, type}) => {
+const Alert = ({ text, type }) => {
     return (
         <div className={`alert alert-${type || 'secondary'} alert-dismissible`} role="alert">
             {text}
-            {/* <strong>Holy guacamole!</strong> You should check in on some of those fields below. */}
             <button type="button" className="close" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
